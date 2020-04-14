600a540d924c15ece0dd75be4a8bf12f7cf015ee
xeeny <xeeny@wp.pl>
2020-04-14 02:13:45 +0200

    added a battery status to status bar

--- slstatus/config.h
+++ slstatus/config.h
@@ -6,6 +6,9 @@ static const char unknown_str[] = "n/a";
  * battery_state       battery charging state          battery name (BAT0)
  * battery_remaining   battery remaining HH:MM         battery name (BAT0) */
 static const struct arg args[] = {
-  { run_command, "%s ", "amixer get Master | tail -n1 | sed -r 's/.*\\[(.*)%\\].*/\\1/'" },
-	{ datetime,    "%s", "%F %T" },
+  { battery_state,     "%s",    "BAT0" },
+  { battery_perc,      "%s%% ", "BAT0" },
+  { battery_remaining, "%s ",   "BAT0" },
+  { run_command,       "%s%% ", "amixer get Master | tail -n1 | sed -r 's/.*\\[(.*)%\\].*/\\1/'" },
+	{ datetime,          "%s",    "%F %T" },
 };

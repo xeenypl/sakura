99b6df36e3b2811b9e5b77a20b1e711391f1705d
xeeny <xeeny@wp.pl>
2020-04-14 11:35:19 +0200

    ignore battery no desktop

--- slstatus/config.h
+++ slstatus/config.h
@@ -1,14 +1,14 @@
 const unsigned int interval = 1000;
-static const char unknown_str[] = "n/a";
+static const char unknown_str[] = "";
 #define MAXLEN 2048
 
 /* battery_perc        battery percentage              battery name (BAT0)
  * battery_state       battery charging state          battery name (BAT0)
  * battery_remaining   battery remaining HH:MM         battery name (BAT0) */
 static const struct arg args[] = {
-  { battery_state,     "%s",    "BAT0" },
-  { battery_perc,      "%s%% ", "BAT0" },
-  { battery_remaining, "%s ",   "BAT0" },
-  { run_command,       "%s%% ", "amixer get Master | tail -n1 | sed -r 's/.*\\[(.*)%\\].*/\\1/'" },
-	{ datetime,          "%s",    "%F %T" },
+  { battery_state,     "%s",  "BAT0" },
+  { battery_perc,      "%s ", "BAT0" },
+  { battery_remaining, "%s ", "BAT0" },
+  { run_command,       "%s ", "amixer get Master | tail -n1 | sed -r 's/.*\\[(.*)%\\].*/\\1/'" },
+	{ datetime,          "%s",  "%F %T" },
 };

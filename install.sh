e553b6309054ac4b9ef73f809079c2e8f82be455
xeeny <xeeny@wp.pl>
2020-04-19 10:01:58 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ git clone https://aur.archlinux.org/yay.git /tmp/yay
 cd /tmp/yay
 makepkg -si
 
-yay -S brave-bin nnn sxiv neovim picom fish cmus mpv ttf-symbola ttf-dejavu openssh sshfs \
-  alsa-utils xorg-setxkbmap xcape xwallpaper xorg-xinit xorg-server libxinerama libxft \
-  adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts \
-  adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts
+yay -S qutebrowser brave-bin nnn sxiv neovim picom fish cmus mpv ttf-symbola \
+  ttf-dejavu openssh sshfs alsa-utils xorg-setxkbmap xcape xwallpaper \
+  xorg-xinit xorg-server libxinerama libxft adobe-source-han-sans-cn-fonts \
+  adobe-source-han-sans-tw-fonts adobe-source-han-sans-jp-fonts \
+  adobe-source-han-sans-kr-fonts

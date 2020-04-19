9e9e9a30d08ea88f1b6dbcf08425819af6c9a6a1
xeeny <xeeny@wp.pl>
2020-04-19 18:31:39 +0200

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,33 +1,13 @@
 # Start X at login
 status is-login && [ -z "$DISPLAY" -a $XDG_VTNR = 1 ] &&  exec startx -- -keeptty
 
+set PATH $HOME/.config/scripts $HOME/.local/bin $PATH
+
 # disale useless stus
 function fish_greeting; end
 function fish_mode_prompt; end
 
 # seting up prompt
-if not set -q __fish_git_prompt_show_informative_status
-    set -g __fish_git_prompt_show_informative_status 1
-end
-if not set -q __fish_git_prompt_color_branch
-    set -g __fish_git_prompt_color_branch brmagenta
-end
-if not set -q __fish_git_prompt_showupstream
-    set -g __fish_git_prompt_showupstream "informative"
-end
-if not set -q __fish_git_prompt_showdirtystate
-    set -g __fish_git_prompt_showdirtystate "yes"
-end
-if not set -q __fish_git_prompt_color_stagedstate
-    set -g __fish_git_prompt_color_stagedstate yellow
-end
-if not set -q __fish_git_prompt_color_invalidstate
-    set -g __fish_git_prompt_color_invalidstate red
-end
-if not set -q __fish_git_prompt_color_cleanstate
-    set -g __fish_git_prompt_color_cleanstate brgreen
-end
-
 function fish_prompt
   set error $status
   set mode --bold red
@@ -51,41 +31,6 @@ function fish_prompt
   end
 end
 
-set PATH /bin
-set PATH /sbin $PATH
-set PATH /usr/local/sbin $PATH
-set PATH /usr/local/bin $PATH
-set PATH /usr/bin $PATH
-set PATH /usr/bin/site_perl $PATH
-set PATH /usr/bin/vendor_perl $PATH
-set PATH /usr/bin/core_perl $PATH
-
-set PATH $HOME/.cargo/bin/ $PATH
-set PATH $HOME/.fzf/bin $PATH
-set PATH $HOME/.config/scripts $PATH
-set PATH $HOME/.local/share/ponyup/bin $PATH
-
-bind -M insert \cr 'fzy_select_history (commandline -b)'
-bind -M insert \cf 'fzy_select_directory'
-
-function fzy_select_history
-    if test (count $argv) = 0
-        set fzy_flags
-    else
-        set fzy_flags -q "$argv"
-    end
-    history|fzy $fzy_flags|read -l foo
-    commandline -f repaint
-    commandline $foo
-end
-
-function fzy_select_directory -d 'cd to a directory using fzy'
-    fd . -t d | fzy | read -l foo
-    if [ $foo ]
-        cd $foo
-    end
-    commandline -f repaint
-end
 
 function l
     ls -al $argv
@@ -95,64 +40,6 @@ function md
     mkdir -p $argv
 end
 
-function ytmp3
-    youtube-dl --ignore-errors  --yes-playlist --extract-audio --audio-format mp3 --output "%(title)s.%(ext)s" $argv
-end
-
-function py
-    python $argv
-end
-
-function dl
-	youtube-dl --ignore-errors --yes-playlist --extract-audio --audio-format mp3 --output "%(uploader)s/%(title)s.%(ext)s" $argv
-end
-
-function toradd
-  transmission-remote -a $argv
-end
-
 function tordel
   transmission-remote -l | awk ' $2 == "100%" && $4 "Done" {print $1}' | xargs -n 1 -I % transmission-remote -t % -r
-end
-
-function man --description 'Format and display manual pages'
-	set -q man_blink; and set -l blink (set_color $man_blink); or set -l blink (set_color -o green)
-    set -q man_bold; and set -l bold (set_color $man_bold); or set -l bold (set_color -o red)
-    set -q man_standout; and set -l standout (set_color $man_standout); or set -l standout (set_color blue)
-    set -q man_underline; and set -l underline (set_color $man_underline); or set -l underline (set_color -u red)
-
-    set -l end (printf "\e[0m")
-
-    set -lx LESS_TERMCAP_mb $blink
-    set -lx LESS_TERMCAP_md $bold
-    set -lx LESS_TERMCAP_me $end
-    set -lx LESS_TERMCAP_so $standout
-    set -lx LESS_TERMCAP_se $end
-    set -lx LESS_TERMCAP_us $underline
-    set -lx LESS_TERMCAP_ue $end
-    set -lx LESS '-R -s'
-
-    set -lx GROFF_NO_SGR yes # fedora
-
-    set -lx MANPATH (string join : $MANPATH)
-    if test -z "$MANPATH"
-        type -q manpath
-        and set MANPATH (command manpath)
-    end
-
-    # Check data dir for Fish 2.x compatibility
-    set -l fish_data_dir
-    if set -q __fish_data_dir
-        set fish_data_dir $__fish_data_dir
-    else
-        set fish_data_dir $__fish_datadir
-    end
-
-    set -l fish_manpath (dirname $fish_data_dir)/fish/man
-    if test -d "$fish_manpath" -a -n "$MANPATH"
-        set MANPATH "$fish_manpath":$MANPATH
-        command man $argv
-        return
-    end
-    command man $argv
 end

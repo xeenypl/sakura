485c8ba3cee34325f3f0a42028a857664667023d
xeeny <xeeny@wp.pl>
2020-04-24 22:57:03 +0200

    update

--- st/config.h
+++ st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "Liberation Mono:pixelsize=20:antialias=true:autohint=true";
-static char *font = "Mono:pixelsize=15:antialias=true:autohint=true";
+static char *font = "Mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
